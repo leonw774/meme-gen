2864285e1d164c13507ae319cccaf9729a9a32a6
leonw774 <leonw774@gmail.com>
2018-06-03 01:16:00 +0800

    Update get_some_meme_from_ineternet.py

--- get_some_memes_from_internet.py
+++ get_some_memes_from_internet.py
@@ -6,13 +6,22 @@ ignore_tag_list = [
 "小叮噹",
 "塊陶太郎",
 "吹捧",
+"嘲諷",
+"吳敦義",
+"淫蕩",
+"政治",
+""
 ]
 ignore_tag_list = [ ("\">" + s + "</span>") for s in ignore_tag_list]
 
+ignore_cap_list = [
+"你", "我", "他"
+]
+
 caption_length_min_limit = 8
 
-p = 1
-for _ in range(50) :
+p = 5
+for _ in range(1000) :
     p += 1
     fname = str(p)
     #print("examing: ", fname)
@@ -32,17 +41,17 @@ for _ in range(50) :
     caption_beg = title_content.find("t=\"") + 3
     caption_end = title_content.find("\" /")
     caption_string = title_content[caption_beg : caption_end]
-    if len(caption_string) < caption_length_min_limit : 
-        continue
+    if any(x in caption_string for x in ignore_cap_list) : continue
+    if len(caption_string) < caption_length_min_limit : continue
     
     pic_beg = web_content.find("[img]") + 5
     pic_end = web_content.find("[/img]")
     if pic_end < 0 : continue
     img_url = web_content[pic_beg : pic_end]
-    if img_url.find(".gif") > 1 : continue
+    if (".gif" in img_url or ".GIF" in img_url) : continue
     #print(img_url)
     img = request.URLopener()
-    img.retrieve(img_url, "./images/" + str(p) + img_url[-4: ])
+    img.retrieve(img_url, "./images/" + str(p) + (img_url[-4: ]).lower())
     
     f = open("./captions/" + fname + ".txt", 'w+', encoding = "utf-8-sig")
     f.write(caption_string)
